37e50ef933f7fa0dc55eb622b55e2e9e01bc339f
Mona Lisa <61238316+HassanIsmailUpdate@users.noreply.github.com>
2021-02-24 22:23:38 +0200

    footer and margin

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
         <CountryPicker handleCountryChange={this.handleCountryChange}/>
         <Cards data={data}/>
         <Chart  data={data} country={country} />
-        <a href="https://www.linkedin.com/in/hassan-ismail-" target="blank"> <footer>Simple React COVID-19 App - hismail</footer> </a> 
+        <footer>Simple React COVID-19 App<a href="https://www.linkedin.com/in/hassan-ismail-" target="blank">hismail</a> </footer> 
       </div>
     )
   };
